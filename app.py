a81e61bb15a4674833fb933709bb049f3680d27f
Piao <asd123698745tw@gmail.com>
2023-01-20 22:56:05 +0800

    modified:   app.py 	modified:   newproject/__init__.py 	new file:   newproject/__pycache__/admin.cpython-311.pyc 	modified:   newproject/__pycache__/models.cpython-311.pyc 	new file:   newproject/admin.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, request, url_for, flash, abort
 from flask_login import login_user, logout_user, login_required
-from newproject import app, db
+from newproject import app, db, admin
 from newproject.models import User
 from newproject.forms import LoginForm, RegistrationForm
 
--- newproject/__init__.py
+++ newproject/__init__.py
@@ -3,13 +3,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
+from flask_admin import Admin
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SECRET_KEY']= 'ffsdfsddsbr'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite3')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#指定ADMIN樣式
+app.config['FLASK_ADMIN_SWATCH']='cerulean'
+#初始化ADMIN
+admin = Admin(app, name='就是後台', template_mode='bootstrap3')
+
+
 
 db = SQLAlchemy(app)
 Migrate(app,db)
